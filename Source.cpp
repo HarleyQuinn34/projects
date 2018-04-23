27ad957fcfd1cde5133c976287b3399082d43cf8
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-04-23 12:41:41 -0600

    pooong

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-﻿#include <allegro5/allegro.h>
+#include <allegro5/allegro.h>
 #include <allegro5\allegro_primitives.h>
 #include <stdio.h>
 #include<allegro5/allegro_font.h>
@@ -6,6 +6,7 @@
 #include<iostream>
 #include<allegro5/allegro_audio.h>
 #include<allegro5/allegro_acodec.h>
+#include <fstream>
 //function declaration
 bool PixelCollision(int x1, int y1, int w1, int hl, int x2, int y2, int w2, int h2);
 
@@ -14,115 +15,44 @@ using namespace std;
 const float FPS = 60;
 const int SCREEN_W = 640;
 const int SCREEN_H = 480;
-const int paddle1_SIZE = 32;
-int score = 0;
+const int BOUNCER_SIZE = 32;
+///////////////////////////////////////////////////////////I need the score
+enum MYKEYS {
+	KEY_UP, KEY_DOWN
+};
 enum MYKEYS2 {
-	KEY_A, KEY_D
+	KEY_W, KEY_S
 };
-
-class brick { //class definition
-private:
-	int xpos;
-	int ypos;
-	bool isdead;
-public:
-	void initbrick(int x, int y);
-	void drawbrick();
-	bool brickcollision(int ballx, int bally, int ballw, int ballh);
-	void kill();
-	bool dead(); //added by mo
-};
-
-//
 int main() {
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
 	ALLEGRO_FONT *font = NULL;
-	ALLEGRO_BITMAP *paddle2 = NULL;
-	ALLEGRO_BITMAP *ball = NULL;
+	ALLEGRO_BITMAP *bouncer = NULL;
+	ALLEGRO_BITMAP *bouncer2 = NULL;
+	ALLEGRO_BITMAP *bouncer3 = NULL;
 	ALLEGRO_SAMPLE *sample = NULL;
 	ALLEGRO_SAMPLE *sample2 = NULL;
 	ALLEGRO_SAMPLE_INSTANCE *sample2Instance = NULL;
 
-	//instantiate (stamp) objects of class brick
-	brick b1;
-	brick b2;
-	brick b3;
-	brick b4;
-	brick b5;
-	brick b6;
-	brick b7;
-	brick b8;
-	brick b9;
-	brick b10;
-	brick b11;
-	brick b12;
-	brick b13;
-	brick b14;
-	brick b15;
-	brick b16;
-	brick b17;
-	brick b18;
-	brick b19;
-	brick b20;
+	float bouncer_x = SCREEN_W / 1.1 - BOUNCER_SIZE / 9.0;
+	float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+	float bouncer_dx = -4.0, bouncer_dy = 4.0;
 
-	//initialize (put data in)
-	b1.initbrick(500, 50);
+	float bouncer2_x = SCREEN_W / 11.0 - BOUNCER_SIZE / 2.0;
+	float bouncer2_y = SCREEN_H / 11.0 - BOUNCER_SIZE / 2.0;
+	float bouncer2_dx = -4.0, bouncer2_dy = 4.0;
 
-	b2.initbrick(400, 50);
+	float bouncer3_x = 200;
+	float bouncer3_y = 200;
+	float bouncer3_dx = -40.0, bouncer3_dy = 4.0;
 
-	b3.initbrick(300, 50);
-
-	b4.initbrick(200, 50);
-
-	b5.initbrick(100, 50);
-
-	b6.initbrick(500, 100);
-
-	b7.initbrick(400, 100);
-
-	b8.initbrick(300, 100);
-
-	b9.initbrick(200, 100);
-
-	b10.initbrick(100, 100);
-
-	b11.initbrick(500, 150);
-
-	b12.initbrick(400, 150);
-
-	b13.initbrick(300, 150);
-
-	b14.initbrick(200, 150);
-
-	b15.initbrick(100, 150);
-
-	b16.initbrick(500, 200);
-
-	b17.initbrick(400, 200);
-
-	b18.initbrick(300, 200);
-
-	b19.initbrick(200, 200);
-
-	b20.initbrick(100, 200);
-
-
-	//paddle starting points
-	float paddle2_x = SCREEN_W / 2;
-	float paddle2_y = SCREEN_H / 1.1 - paddle1_SIZE / 2.0;
-
-	float paddle2_dx = -8.0, paddle2_dy = 8.0;
-
-	float ball_x = 200;
-	float ball_y = 300;
-	float ball_dx = -8.0, ball_dy = 8.0;
-
+	bool key[4] = { false, false, false, false };
 	bool key2[4] = { false, false, false, false };
 	bool redraw = true;
 	bool doexit = false;
-
+	int score = -1;
+	int score2 = -1;
 	al_init();
 	al_install_keyboard();
 	al_init_primitives_addon();
@@ -148,16 +78,17 @@ int main() {
 	al_attach_sample_instance_to_mixer(sample2Instance, al_get_default_mixer());
 
 
+	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
+	bouncer2 = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
+	bouncer3 = al_create_bitmap(15, 15);
 
-	paddle2 = al_create_bitmap(paddle1_SIZE, paddle1_SIZE);
-	ball = al_create_bitmap(15, 15);
-
-
-
-	al_set_target_bitmap(paddle2);
+	al_set_target_bitmap(bouncer);
 	al_clear_to_color(al_map_rgb(255, 255, 255));
 
-	al_set_target_bitmap(ball);
+	al_set_target_bitmap(bouncer2);
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+
+	al_set_target_bitmap(bouncer3);
 	al_clear_to_color(al_map_rgb(255, 255, 255));
 	al_set_target_bitmap(al_get_backbuffer(display));
 
@@ -170,191 +101,122 @@ int main() {
 	al_flip_display();
 	al_start_timer(timer);
 
+	int topscores[5];
+	int highscores[5];
+	ifstream infile;
+	ifstream scoreread;
+	int num;
+	infile.open("save.txt");
+	int temp;
+	//reads in from file
+	for (int i = 0; i < 5; i++)
+		infile >> topscores[i];
 
-	while (!doexit) {//game loop
+	//print to console for testing
+	for (int i = 0; i < 5; i++)
+		cout << topscores[i] << endl;
+
+	al_draw_text(font, al_map_rgb(250, 250, 250), 150, 20, ALLEGRO_ALIGN_CENTER, "Top Scores");
+	//print from screen
+	for (int i = 0; i < 5; i++)
+	al_draw_textf(font, al_map_rgb(250, 250, 250), 500, 50+i*40, ALLEGRO_ALIGN_CENTER, "%d", topscores[i]);
+
+	al_flip_display();
+	al_rest(5);
+	while (!doexit) {
 
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
 
-		//physics engine
 		if (ev.type == ALLEGRO_EVENT_TIMER) {//note from mo: this was in the wrong place... all physics should be *inside* the timer section
-
-
-			if (ball_x < 0 || ball_x > 640 - 10) { //has ball hit left or right wall
-				ball_dx = -ball_dx;//flip direction
-
+			if (bouncer3_x < 0) {
+				bouncer3_dx = -bouncer3_dx;
+				score++;
+			}
+				
+				if(bouncer3_x > 640 - 10) {
+				bouncer3_dx = -bouncer3_dx;
+				score2++;
 			}
 
 
-			if (ball_y < 0 || ball_y > 480 - 10) {//has ball hit top or bottom wall
-				ball_dy = -ball_dy; //flip direction
+			if (bouncer3_y < 0 || bouncer3_y > 480 - 10) {
+				bouncer3_dy = -bouncer3_dy;
 			}
 			//add the x velocity to the x position, 
 			//and the y velocity to the y position
-			ball_x += ball_dx;
-			ball_y += ball_dy;
-
-
-
-			//note from mo: timer section was here, moved up above
-
-
-			//paddle movement and speed
-			if (key2[KEY_A]) {
-				paddle2_x -= 12.0;
-			}
-
-			if (key2[KEY_D]) {
-				paddle2_x += 12.0;
-			}
-
-
+			bouncer3_x += bouncer3_dx;
+			bouncer3_y += bouncer3_dy;
 
 			redraw = true;
 
-			//collide ball with paddle
-			if (PixelCollision(ball_x, ball_y, 15, 15, paddle2_x, paddle2_y, 100, 30)) {
+			//note from mo: timer section was here, moved up above
+
+			if (key2[KEY_W]) {
+				bouncer2_y -= 4.0;
+			}
+
+			if (key2[KEY_S]) {
+				bouncer2_y += 4.0;
+			}
+
+			if (key[KEY_UP]) {
+				bouncer_y -= 4.0;
+			}
+
+			if (key[KEY_DOWN]) {
+				bouncer_y += 4.0;
+			}
+
+			redraw = true;
+
+
+			if (PixelCollision(bouncer3_x, bouncer3_y, 15, 15, bouncer_x, bouncer_y, 32, 100)) {
 				cout << "collision!";
 				al_play_sample_instance(sample2Instance);
-				//ball_dx = -ball_dx;//reflection
-				ball_dy = -ball_dy;
-			}
+				//al_play_sample(sample2, 2, 5.0, 1, ALLEGRO_PLAYMODE_ONCE, NULL);
+				bouncer3_dx = -bouncer3_dx;//reflection
 
-			//do this for all yo bricks
-			if (b1.dead() == false && b1.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b1.kill();
+										   //system("pause");
 
 			}
-			if (b2.dead() == false && b2.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b2.kill();
+			if (PixelCollision(bouncer3_x, bouncer3_y, 15, 15, bouncer2_x, bouncer2_y, 32, 100)) {
+				cout << "collision!";
+				//sample = true;
+				al_play_sample_instance(sample2Instance);
+				bouncer3_dx = -bouncer3_dx;//reflection
 
+										   //system("pause");
 			}
-			if (b3.dead() == false && b3.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b3.kill();
 
-			}
-			if (b4.dead() == false && b4.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b4.kill();
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_W:
+				key2[KEY_W] = true;
 
-			}
-			if (b5.dead() == false && b5.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b5.kill();
+				break;
 
-			}
-			if (b6.dead() == false && b6.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b6.kill();
+			case ALLEGRO_KEY_S:
+				key2[KEY_S] = true;
+				break;
+			case ALLEGRO_KEY_UP:
+				key[KEY_UP] = true;
+				break;
 
-			}
-			if (b7.dead() == false && b7.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b7.kill();
-			}
-			if (b8.dead() == false && b8.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b8.kill();
-			}
-			if (b9.dead() == false && b9.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b9.kill();
-			}
-			if (b10.dead() == false && b10.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b10.kill();
-			}
-			if (b11.dead() == false && b11.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b11.kill();
-
-			}
-			if (b12.dead() == false && b12.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b12.kill();
-
-			}
-			if (b13.dead() == false && b13.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b13.kill();
-
-			}
-			if (b14.dead() == false && b14.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b14.kill();
-
-			}
-			if (b15.dead() == false && b15.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b15.kill();
-
-			}
-			if (b16.dead() == false && b16.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b16.kill();
-
-			}
-			if (b17.dead() == false && b17.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b17.kill();
-			}
-			if (b18.dead() == false && b18.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b18.kill();
-			}
-			if (b19.dead() == false && b18.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b19.kill();
-			}
-			if (b20.dead() == false && b20.brickcollision(ball_x, ball_y, 32, 32) == true) {//changed to == from =
-				ball_dy = -ball_dy;
-				//ball_dx = -ball_dx;
-				b20.kill();
+			case ALLEGRO_KEY_DOWN:
+				key[KEY_DOWN] = true;
+				break;
 			}
 		}
-		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) { //you've pressed a key
+		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
 			switch (ev.keyboard.keycode) {
-			case ALLEGRO_KEY_A:
-				key2[KEY_A] = true;
-
+			case ALLEGRO_KEY_W:
+				key2[KEY_W] = false;
 				break;
 
-			case ALLEGRO_KEY_D:
-				key2[KEY_D] = true;
-				break;
-
-			}
-		}
-		else if (ev.type == ALLEGRO_EVENT_KEY_UP) { //you've let go of a key
-			switch (ev.keyboard.keycode) {
-			case ALLEGRO_KEY_A:
-				key2[KEY_A] = false;
-				break;
-
-			case ALLEGRO_KEY_D:
-				key2[KEY_D] = false;
+			case ALLEGRO_KEY_S:
+				key2[KEY_S] = false;
 				break;
 
 
@@ -362,68 +224,55 @@ int main() {
 				doexit = true;
 				break;
 
+			case ALLEGRO_KEY_UP:
+				key[KEY_UP] = false;
+				break;
 
+			case ALLEGRO_KEY_DOWN:
+				key[KEY_DOWN] = false;
+				break;
 			}
 		}
 		//render section//////////////////////////////////////////////////////////////////////////////
 		if (redraw && al_is_event_queue_empty(event_queue)) {
 			redraw = false;
 
-			//clears off screen
-
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 
 
-			al_draw_filled_rectangle(paddle2_x, paddle2_y, paddle2_x + 100, paddle2_y + 30, al_map_rgb(255, 255, 255));
-			al_draw_bitmap(ball, ball_x, ball_y, 0);
+			al_draw_filled_rectangle(bouncer_x, bouncer_y, bouncer_x + 30, bouncer_y + 100, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(bouncer2_x, bouncer2_y, bouncer2_x + 30, bouncer2_y + 100, al_map_rgb(255, 255, 255));
+			al_draw_bitmap(bouncer3, bouncer3_x, bouncer3_y, 0);
+			al_draw_textf(font, al_map_rgb(250, 250, 250), 150, 50, ALLEGRO_ALIGN_CENTER, "%d", score);
+			al_draw_textf(font, al_map_rgb(250, 250, 250), 500, 50, ALLEGRO_ALIGN_CENTER, "%d",score2);
 
-
-
-			if (b1.dead() == false)
-				b1.drawbrick();
-			if (b2.dead() == false)
-				b2.drawbrick();
-			if (b3.dead() == false)
-				b3.drawbrick();
-			if (b4.dead() == false)
-				b4.drawbrick();
-			if (b5.dead() == false)
-				b5.drawbrick();
-			if (b6.dead() == false)
-				b6.drawbrick();
-			if (b7.dead() == false)
-				b7.drawbrick();
-			if (b8.dead() == false)
-				b8.drawbrick();
-			if (b9.dead() == false)
-				b9.drawbrick();
-			if (b10.dead() == false)
-				b10.drawbrick();
-			if (b11.dead() == false)
-				b11.drawbrick();
-			if (b12.dead() == false)
-				b12.drawbrick();
-			if (b13.dead() == false)
-				b13.drawbrick();
-			if (b14.dead() == false)
-				b14.drawbrick();
-			if (b15.dead() == false)
-				b15.drawbrick();
-			if (b16.dead() == false)
-				b16.drawbrick();
-			if (b17.dead() == false)
-				b17.drawbrick();
-			if (b18.dead() == false)
-				b18.drawbrick();
-			if (b19.dead() == false)
-				b19.drawbrick();
-			if (b20.dead() == false)
-				b20.drawbrick();
 			al_flip_display();
-		}//end render section
-	}//end game loop
-	al_destroy_bitmap(paddle2);
-	al_destroy_bitmap(ball);
+		}
+	}
+
+
+	ofstream write;
+	write.open("save.txt");
+
+	for (int i = 0; i < 5; i++) {
+		cout << "highscore is " << topscores[i];
+		if (score > topscores[i]) {
+			temp = topscores[i];
+			topscores[i] = score;
+			score = temp;
+			cout << "replacing score" << endl;
+		}
+	}
+	//this part actually writes to the file
+	for (int i = 0; i < 5; i++) {
+		write << topscores[i] << endl;
+	}
+	al_rest(2);
+	write.close();
+
+	al_destroy_bitmap(bouncer);
+	al_destroy_bitmap(bouncer2);
+	al_destroy_bitmap(bouncer3);
 	al_destroy_timer(timer);
 	al_destroy_display(display);
 	al_destroy_event_queue(event_queue);
@@ -440,37 +289,4 @@ bool PixelCollision(int x1, int y1, int w1, int hl, int x2, int y2, int w2, int 
 
 	else
 		return false;
-}
-void brick::drawbrick() {
-	al_draw_rectangle(xpos, ypos, xpos + 100, ypos + 20, al_map_rgb(rand(), rand(), rand()), 5);
-}
-
-void brick::initbrick(int x, int y) {
-
-	xpos = x;
-	ypos = y;
-	isdead = false;
-}
-bool brick::brickcollision(int ballx, int bally, int ballw, int ballh) {
-
-	if ((xpos + 100 > ballx) && (ballx + 32 > xpos) && (bally + 32 > ypos) && (ypos + 32 > bally))
-
-		return true;
-
-	else
-		return false;
-}
-
-//added by mo
-bool brick::dead() {
-
-	return isdead;
-
-}
-
-void  brick::kill() {
-
-	isdead = true;
-
-
 }
