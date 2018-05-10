b767a1311f0594fdc3defc792c35e20cb04c0e29
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-05-10 14:42:40 -0600

    pacman updated

--- Source.cpp
+++ Source.cpp
@@ -1,97 +1,84 @@
-#include <allegro5/allegro.h>
-#include <allegro5\allegro_primitives.h>
 #include <stdio.h>
-#include<allegro5/allegro_font.h>
-#include<allegro5/allegro_ttf.h>
-#include<iostream>
+#include <allegro5/allegro.h>
+#include<allegro5/allegro_primitives.h>
 #include<allegro5/allegro_audio.h>
 #include<allegro5/allegro_acodec.h>
-#include <fstream>
-//function declaration
-bool PixelCollision(int x1, int y1, int w1, int hl, int x2, int y2, int w2, int h2);
-
+#include<iostream>
 using namespace std;
-
+const int PACSIZE = 20;
 const float FPS = 60;
-const int SCREEN_W = 640;
-const int SCREEN_H = 480;
-const int BOUNCER_SIZE = 32;
-///////////////////////////////////////////////////////////I need the score
+const int SCREEN_W = 570;
+const int SCREEN_H = 615;
+const int BOUNCER_SIZE = 60;
+enum dir {
+	UP, DOWN, LEFT, 
+	RIGHT
+};
 enum MYKEYS {
-	KEY_UP, KEY_DOWN
+	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
 };
-enum MYKEYS2 {
-	KEY_W, KEY_S
-};
-int main() {
+
+int wallCollide(int x, int y, int dir, int level[21][21]);
+
+int main(int argc, char **argv) {
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_BITMAP *bouncer = NULL;
+	ALLEGRO_BITMAP *bouncer2 = NULL; 
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	ALLEGRO_TIMER *timer = NULL;
-	ALLEGRO_FONT *font = NULL;
-	ALLEGRO_BITMAP *bouncer = NULL;
-	ALLEGRO_BITMAP *bouncer2 = NULL;
-	ALLEGRO_BITMAP *bouncer3 = NULL;
-	ALLEGRO_SAMPLE *sample = NULL;
+	ALLEGRO_BITMAP*wall = NULL;
+	//ALLEGRO_SAMPLE *sample = NULL;
 	ALLEGRO_SAMPLE *sample2 = NULL;
 	ALLEGRO_SAMPLE_INSTANCE *sample2Instance = NULL;
+	ALLEGRO_BITMAP*dots = NULL;
 
-	float bouncer_x = SCREEN_W / 1.1 - BOUNCER_SIZE / 9.0;
-	float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+
+	cout << "FLEGIN" << endl;
+	int bouncer_x = 305 - BOUNCER_SIZE / 2;
+	int bouncer_y = 280 - BOUNCER_SIZE / 2;
 	float bouncer_dx = -4.0, bouncer_dy = 4.0;
 
-	float bouncer2_x = SCREEN_W / 11.0 - BOUNCER_SIZE / 2.0;
-	float bouncer2_y = SCREEN_H / 11.0 - BOUNCER_SIZE / 2.0;
-	float bouncer2_dx = -4.0, bouncer2_dy = 4.0;
-
-	float bouncer3_x = 200;
-	float bouncer3_y = 200;
-	float bouncer3_dx = -40.0, bouncer3_dy = 4.0;
+	float bouncer2_x = SCREEN_W / 4.0 - BOUNCER_SIZE / 2.0;
+	float bouncer2_y = SCREEN_H / 4.0 - BOUNCER_SIZE / 2.0;
+	float bouncer2_dx = -2.0, bouncer2_dy = 4.0;
 
 	bool key[4] = { false, false, false, false };
 	bool key2[4] = { false, false, false, false };
 	bool redraw = true;
 	bool doexit = false;
-	int score = -1;
-	int score2 = -1;
 	al_init();
 	al_install_keyboard();
 	al_init_primitives_addon();
-	al_init_font_addon();
-	al_init_ttf_addon();
 	al_install_audio();
 	al_init_acodec_addon();
+	
 	timer = al_create_timer(1.0 / FPS);
 	display = al_create_display(SCREEN_W, SCREEN_H);
-
-	font = al_load_ttf_font("cubic.ttf", 20, 0);
 	al_reserve_samples(2);
-	sample2 = al_load_sample("8bit.wav");
-	sample = al_load_sample("oof.wav");
+	sample2 = al_load_sample("pac.wav");
+	//sample = al_load_sample("oof.wav");
 
 	sample2Instance = al_create_sample_instance(sample2);
 	al_set_sample_instance_playmode(sample2Instance, ALLEGRO_PLAYMODE_LOOP);
 	al_attach_sample_instance_to_mixer(sample2Instance, al_get_default_mixer());
 	al_play_sample_instance(sample2Instance);
 
-	sample2Instance = al_create_sample_instance(sample);
-	al_set_sample_instance_playmode(sample2Instance, ALLEGRO_PLAYMODE_ONCE);
-	al_attach_sample_instance_to_mixer(sample2Instance, al_get_default_mixer());
-
-
+	dots = al_create_bitmap(5, 5);
+	al_set_target_bitmap(dots);
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+	//////////////////////
+	wall = al_create_bitmap(30, 30);////////////////////wall size
+	al_set_target_bitmap(wall);
+	al_clear_to_color(al_map_rgb(128, 0, 0));
+	/////////////////////////////////
 	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
 	bouncer2 = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-	bouncer3 = al_create_bitmap(15, 15);
 
 	al_set_target_bitmap(bouncer);
-	al_clear_to_color(al_map_rgb(255, 255, 255));
-
+	al_clear_to_color(al_map_rgb(255, 255, 0));
 	al_set_target_bitmap(bouncer2);
 	al_clear_to_color(al_map_rgb(255, 255, 255));
-
-	al_set_target_bitmap(bouncer3);
-	al_clear_to_color(al_map_rgb(255, 255, 255));
 	al_set_target_bitmap(al_get_backbuffer(display));
-
 	event_queue = al_create_event_queue();
 
 	al_register_event_source(event_queue, al_get_display_event_source(display));
@@ -100,106 +87,69 @@ int main() {
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 	al_flip_display();
 	al_start_timer(timer);
+	int level[21][21] = {
+		1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,
+		1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,1,0,0,1,1,1,1,1,0,0,1,1,1,0,1,
+		1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,1,0,0,1,
+		1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1,1,0,0,1,
+		1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1,0,0,0,1,
+		1,0,0,1,0,0,1,0,0,1,0,0,0,1,0,0,1,0,0,0,1,
+		1,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,1,1,1,0,1,
+		1,0,0,1,0,0,0,0,0,1,1,1,1,1,0,0,1,0,0,0,1,
+		1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,
+		1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1,0,1,0,1,
+		1,0,0,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,1,0,1,
+		1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1, };
+	for (int i = 0; i < 21; i++) {
+		for (int j = 0; j < 21; j++)
+			cout << level[i][j];
+		cout << endl;
 
-	int topscores[5];
-	int highscores[5];
-	ifstream infile;
-	ifstream scoreread;
-	int num;
-	infile.open("save.txt");
-	int temp;
-	//reads in from file
-	for (int i = 0; i < 5; i++)
-		infile >> topscores[i];
+	}
 
-	//print to console for testing
-	for (int i = 0; i < 5; i++)
-		cout << topscores[i] << endl;
 
-	al_draw_text(font, al_map_rgb(250, 250, 250), 150, 20, ALLEGRO_ALIGN_CENTER, "Top Scores");
-	//print from screen
-	for (int i = 0; i < 5; i++)
-	al_draw_textf(font, al_map_rgb(250, 250, 250), 500, 50+i*40, ALLEGRO_ALIGN_CENTER, "%d", topscores[i]);
-
-	al_flip_display();
-	al_rest(5);
-	while (!doexit) {
-
+	while (!doexit)
+	{
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
+		if (ev.type == ALLEGRO_EVENT_TIMER) {
 
-		if (ev.type == ALLEGRO_EVENT_TIMER) {//note from mo: this was in the wrong place... all physics should be *inside* the timer section
-			if (bouncer3_x < 0) {
-				bouncer3_dx = -bouncer3_dx;
-				score++;
-			}
-				
-				if(bouncer3_x > 640 - 10) {
-				bouncer3_dx = -bouncer3_dx;
-				score2++;
-			}
-
-
-			if (bouncer3_y < 0 || bouncer3_y > 480 - 10) {
-				bouncer3_dy = -bouncer3_dy;
-			}
-			//add the x velocity to the x position, 
-			//and the y velocity to the y position
-			bouncer3_x += bouncer3_dx;
-			bouncer3_y += bouncer3_dy;
-
-			redraw = true;
-
-			//note from mo: timer section was here, moved up above
-
-			if (key2[KEY_W]) {
-				bouncer2_y -= 4.0;
-			}
-
-			if (key2[KEY_S]) {
-				bouncer2_y += 4.0;
-			}
-
-			if (key[KEY_UP]) {
+			if (key[KEY_UP] && bouncer_y >= 4.0) {
 				bouncer_y -= 4.0;
 			}
 
-			if (key[KEY_DOWN]) {
+			if (key[KEY_DOWN] && bouncer_y <= SCREEN_H - BOUNCER_SIZE - 4.0) {
 				bouncer_y += 4.0;
 			}
 
+			//go right
+			
+			if (key[KEY_RIGHT] && bouncer_x <= SCREEN_W - BOUNCER_SIZE - 4.0) {
+				bouncer_x += 4.0;
+			}
+
+			if (key[KEY_LEFT] && bouncer_x > 4) {
+				bouncer_x -= 4.0;
+			}
+			if (level[bouncer_x / 30][bouncer_y / 30] == 0) {
+				level[bouncer_x / 30][bouncer_y / 30] = 2;
+			}
 			redraw = true;
-
-
-			if (PixelCollision(bouncer3_x, bouncer3_y, 15, 15, bouncer_x, bouncer_y, 32, 100)) {
-				cout << "collision!";
-				al_play_sample_instance(sample2Instance);
-				//al_play_sample(sample2, 2, 5.0, 1, ALLEGRO_PLAYMODE_ONCE, NULL);
-				bouncer3_dx = -bouncer3_dx;//reflection
-
-										   //system("pause");
-
-			}
-			if (PixelCollision(bouncer3_x, bouncer3_y, 15, 15, bouncer2_x, bouncer2_y, 32, 100)) {
-				cout << "collision!";
-				//sample = true;
-				al_play_sample_instance(sample2Instance);
-				bouncer3_dx = -bouncer3_dx;//reflection
-
-										   //system("pause");
-			}
-
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break;
 		}
 		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
 			switch (ev.keyboard.keycode) {
-			case ALLEGRO_KEY_W:
-				key2[KEY_W] = true;
 
-				break;
 
-			case ALLEGRO_KEY_S:
-				key2[KEY_S] = true;
-				break;
 			case ALLEGRO_KEY_UP:
 				key[KEY_UP] = true;
 				break;
@@ -207,17 +157,18 @@ int main() {
 			case ALLEGRO_KEY_DOWN:
 				key[KEY_DOWN] = true;
 				break;
+
+			case ALLEGRO_KEY_LEFT:
+				key[KEY_LEFT] = true;
+				break;
+
+			case ALLEGRO_KEY_RIGHT:
+				key[KEY_RIGHT] = true;
+				break;
 			}
 		}
 		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
 			switch (ev.keyboard.keycode) {
-			case ALLEGRO_KEY_W:
-				key2[KEY_W] = false;
-				break;
-
-			case ALLEGRO_KEY_S:
-				key2[KEY_S] = false;
-				break;
 
 
 			case ALLEGRO_KEY_ESCAPE:
@@ -231,48 +182,47 @@ int main() {
 			case ALLEGRO_KEY_DOWN:
 				key[KEY_DOWN] = false;
 				break;
+
+			case ALLEGRO_KEY_LEFT:
+				key[KEY_LEFT] = false;
+				break;
+
+			case ALLEGRO_KEY_RIGHT:
+				key[KEY_RIGHT] = false;
+				break;
+
+				/*case ALLEGRO_KEY_ESCAPE:
+				doexit = true;
+				break;*/
 			}
 		}
+
+
+
+
 		//render section//////////////////////////////////////////////////////////////////////////////
 		if (redraw && al_is_event_queue_empty(event_queue)) {
 			redraw = false;
 
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 
+			al_draw_filled_circle(bouncer_x, bouncer_y, 15, al_map_rgb(255, 255, 0));
 
-			al_draw_filled_rectangle(bouncer_x, bouncer_y, bouncer_x + 30, bouncer_y + 100, al_map_rgb(255, 255, 255));
-			al_draw_filled_rectangle(bouncer2_x, bouncer2_y, bouncer2_x + 30, bouncer2_y + 100, al_map_rgb(255, 255, 255));
-			al_draw_bitmap(bouncer3, bouncer3_x, bouncer3_y, 0);
-			al_draw_textf(font, al_map_rgb(250, 250, 250), 150, 50, ALLEGRO_ALIGN_CENTER, "%d", score);
-			al_draw_textf(font, al_map_rgb(250, 250, 250), 500, 50, ALLEGRO_ALIGN_CENTER, "%d",score2);
 
+			for (int i = 0; i < 21; i++)
+				for (int j = 0; j < 21; j++) {
+					if (level[i][j] == 1)
+						al_draw_bitmap(wall, i * 30, j * 30, 0);
+					if (level[i][j] == 0)
+						al_draw_bitmap(dots, i * 30+10, j * 30+10, 0);
+
+				}
 			al_flip_display();
 		}
 	}
 
 
-	ofstream write;
-	write.open("save.txt");
-
-	for (int i = 0; i < 5; i++) {
-		cout << "highscore is " << topscores[i];
-		if (score > topscores[i]) {
-			temp = topscores[i];
-			topscores[i] = score;
-			score = temp;
-			cout << "replacing score" << endl;
-		}
-	}
-	//this part actually writes to the file
-	for (int i = 0; i < 5; i++) {
-		write << topscores[i] << endl;
-	}
-	al_rest(2);
-	write.close();
-
 	al_destroy_bitmap(bouncer);
-	al_destroy_bitmap(bouncer2);
-	al_destroy_bitmap(bouncer3);
 	al_destroy_timer(timer);
 	al_destroy_display(display);
 	al_destroy_event_queue(event_queue);
@@ -280,13 +230,84 @@ int main() {
 
 	return 0;
 }
-//function definition
-bool PixelCollision(int x1, int y1, int w1, int hl, int x2, int y2, int w2, int h2) {
 
-	if ((x1 + w1 > x2) && (x2 + w2 > x1) && (y2 + h2 > y1) && (y1 + hl > y2))
+bool pixelCollision(int x1, int y1, int x2, int y2) {
 
-		return true;
+	if (pixelCollision(x1, y1, x2, y2) == true);
+	/*if (key[3] && !wallCollide(bouncer_x, bouncer_y, RIGHT, wall)) {
+		bouncer_x += 4.0;
+	}*/
+	return false;
 
-	else
-		return false;
+
+
 }
+int wallCollide(int x, int y, int dir, int level[21][21]) {
+	int x1;
+	int x2;
+	int x3;
+	int y1;
+	int y2;
+	int y3;
+	if (dir == RIGHT) { 		// Moving Right
+								// Check along the far right side of the sprite, plus 3 (the amount we’re moving)
+		x1 = x + 5 + PACSIZE;
+		x2 = x + 5 + PACSIZE;
+		x3 = x + 5 + PACSIZE;
+		// Check at three point along that edge
+		y1 = y;
+		y2 = y +PACSIZE / 2;
+		y3 = y +PACSIZE;
+		if ((level[x1 / 30][y1 / 30] == 1) || (level[x2 / 30][y2 / 30] == 1) || (level[x3 / 30][y3 / 30] == 1)) {
+	}
+		cout << "right collision" << endl;
+		return 1;
+
+	}
+	else {
+		return 0;
+
+	}
+	if (dir == LEFT) { 		// Moving Right
+								// Check along the far right side of the sprite, plus 3 (the amount we’re moving)
+		x1 = x - 3;
+		x2 = x - 3;
+		x3 = x - 3;
+		y1 = y;
+		y2 = y + PACSIZE / 2;
+		y3 = y + PACSIZE;
+	}
+	if ((level[x1 / 30][y1 / 30] == 1) || (level[x2 / 30][y2 / 30] == 1) || (level[x3 / 30][y3 / 30] == 1)) {
+	}
+	else {
+		return 0;
+	}
+	if (dir == UP) { 		// Moving Right
+							// Check along the far right side of the sprite, plus 3 (the amount we’re moving)
+		x1 = x;
+		x2 = x + PACSIZE / 2;
+		x3 = x + PACSIZE;
+		y1 = y - 5;
+		y2 = y - 5;
+		y3 = y - 5;
+	}
+	if ((level[x1 / 30][y1 / 30] == 1) || (level[x2 / 30][y2 / 30] == 1) || (level[x3 / 30][y3 / 30] == 1)) {
+	}
+	else {
+		return 0;
+	}
+	if (dir == DOWN) { 		// Moving Right
+		x1 = x;
+		x2 = x + PACSIZE / 2;
+		x3 = x + PACSIZE;
+		y1 = y + PACSIZE + 5;
+		y2 = y + PACSIZE + 5;
+		y3 = y + PACSIZE + 5;
+	}
+	if ((level[x1 / 30][y1 / 30] == 1) || (level[x2 / 30][y2 / 30] == 1) || (level[x3 / 30][y3 / 30] == 1)) {
+	}
+	else {
+		return 0;
+	}
+}
+ 
