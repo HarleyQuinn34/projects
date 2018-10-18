8a988d552aa0c1cab47a5fa3094ba5118f70ccbe
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-10-18 14:41:47 -0600

    text based game (alien)

--- Source.cpp
+++ Source.cpp
@@ -1,210 +1,307 @@
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <allegro5\allegro_image.h>
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <Windows.h>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
+void monster(int health, int ammo);
+//void convert(string & input);
+string array[10];
 
+int main() {
+	int room = 1;
+	bool doexit = false;// exits the game
+	string items[10];// hold my inventory items
+	string input = "a";
+	srand(time(NULL));
+	int health = 100;
+	int ammo = 10;
+	cout << "You are alone" << endl;
+	while (doexit == false) {
 
-const float FPS = 60;
-const int SCREEN_W = 900;
-const int SCREEN_H = 500;
-const int BOUNCER_SIZE = 32;
-int worldWidth = 4128;
-int worldHeight = 3754;
-int cameraX = 0;
-int cameraY = 0;
-enum MYKEYS {
-	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
-};
+		switch (room) {
+		case 1:
+			cout << "YOU'RE IN ROOM 1." << endl;
+			cout << "You are at the bottom of the ship." << endl;
+			cout << "You can go (north) and (west). " << endl;
 
-int main(int argc, char **argv)
-{
-	ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	ALLEGRO_TIMER *timer = NULL;
-	ALLEGRO_BITMAP *bouncer = NULL;
-	ALLEGRO_BITMAP *map = NULL;
-	//float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
-	//float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
-
-	float bouncer_x = 2606 + SCREEN_W;
-	float bouncer_y = 574 + SCREEN_W;
-
-	bool key[4] = { false, false, false, false };
-	bool redraw = true;
-	bool doexit = false;
-	bool onEdge = false;
-
-
-	al_init();
-
-	al_init_image_addon();
-
-	al_install_keyboard();
-	map = al_load_bitmap("ff1.png");
-
-	timer = al_create_timer(1.0 / FPS);
-
-
-	display = al_create_display(SCREEN_W, SCREEN_H);
-
-
-	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-
-
-	al_set_target_bitmap(bouncer);
-
-	al_clear_to_color(al_map_rgb(255, 0, 255));
-
-	al_set_target_bitmap(al_get_backbuffer(display));
-
-	event_queue = al_create_event_queue();
-
-
-	al_register_event_source(event_queue, al_get_display_event_source(display));
-
-	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-
-	al_register_event_source(event_queue, al_get_keyboard_event_source());
-
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-
-	al_flip_display();
-
-	al_start_timer(timer);
-
-	while (!doexit)
-	{
-		ALLEGRO_EVENT ev;
-		al_wait_for_event(event_queue, &ev);
-
-		if (ev.type == ALLEGRO_EVENT_TIMER) {
-
-			cameraX = bouncer_x - SCREEN_W / 2;
-			cameraY = bouncer_y - SCREEN_H / 2;
-
-			if (key[KEY_UP] && bouncer_y < worldHeight + 32) {
-				bouncer_y -= 4.0;
-			}
-
-			if (key[KEY_DOWN] && bouncer_y < worldHeight - 32) {
-				bouncer_y += 4.0;
-			}
-
-			if (key[KEY_LEFT] && bouncer_x < worldWidth - 32) {
-				bouncer_x -= 4.0;
-			}
-
-			if (key[KEY_RIGHT] && bouncer_x < worldWidth - 32) {
-				bouncer_x += 4.0;
-			}
-
-			redraw = true;
-			cout << "onedge is " << onEdge << endl;
-			//left
-			if (cameraX < 0) {
-				cameraX = 0;
-				onEdge = true; 
-			}
-			else 
-				onEdge = false;
-
-
-			//top
-			if (cameraY < 0) {
-				cameraY = 0;
-				onEdge = true;
+			getline(cin, input);
+			if (input.compare("north") == 0)
+				room = 4;
+			else if (input.compare("west") == 0)
+				room = 2;
+			else if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
 			}
 			else
-				onEdge = false;
-
-
-			//right
-			if (cameraX > worldWidth - SCREEN_W) { 
-				cameraX = worldWidth - SCREEN_W;
-				onEdge = true;
+				cout << "not an option unless you are quiting" << endl;
+			break;
+		case 2:
+			cout << "YOU'RE IN ROOM 2." << endl;
+			cout << "Room 2 is a directory and you see a map with all the rooms in the ship.Someone drew somthing on room 10, it says nest." << endl;
+			cout << "You can go (north), (west), and (east). " << endl;
+			if (items[4].compare("handle") != 0)
+				cout << "You find a handle to some sort of weapon, you might need it later. Type (handle) to pick it up." << endl;
+			getline(cin, input);
+			if (input.compare("north") == 0)
+				room = 5;
+			else if (input.compare("west") == 0)
+				room = 3;
+			else if (input.compare("east") == 0)
+				room = 1;
+			else if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
+			}
+			else if (input.compare("handle") == 0) {
+				items[4] = "handle";
+				cout << "You got the handle" << endl;
 			}
 			else
-				onEdge = false;
-
-			//bottom
-			if (cameraY > worldHeight - SCREEN_H) {
-				cameraY = worldHeight - SCREEN_H;
-				onEdge = true;
+				cout << "not an option unless you are quiting" << endl;
+			break;
+		case 3:
+			cout << "YOU'RE IN ROOM 3." << endl;
+			cout << "You look around and relize you are in a computer room.There is desk with a broken computer,phone and other tech ." << endl;
+			cout << "There are pamplets about the lab that was used to be here.Type (read) if you want to read about the ship." << endl;
+			cout << "You can go (north), and (east). " << endl;
+			monster(health, ammo);
+			getline(cin, input);
+			if (input.compare("north") == 0)
+				room = 6;
+			else if (input.compare("east") == 0)
+				room = 2;
+			else if (input.compare("read") == 0)
+				room = 11;
+			else if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
 			}
+
 			else
-				onEdge = false;
+				cout << "not an option unless you are quiting" << endl;
+
+			break;
+		case 4:
+			cout << "YOU'RE IN ROOM 4" << endl;
+			cout << "You look around and see medical aid objects and aid kits,there is a sink in the corner of the room." << endl;
+			cout << "You can go (north), (west), and (south). " << endl;
+			if (items[1].compare("gas") != 0)
+				cout << "You find a gas canister left over from on old mission, you need thes for the flame thrower. Type (gas) to pick it up." << endl;
+			monster(health, ammo);
+			getline(cin, input);
+			if (input.compare("north") == 0)
+				room = 7;
+			else if (input.compare("west") == 0)
+				room = 5;
+			else if (input.compare("south") == 0)
+				room = 1;
+			if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
 			}
-
-			else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-				break;
+			if (input.compare("gas") == 0) {
+				items[1] = "gas";
+				cout << "You got the gas" << endl;
 			}
-			else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-				switch (ev.keyboard.keycode) {
-				case ALLEGRO_KEY_UP:
-					key[KEY_UP] = true;
-					break;
-
-				case ALLEGRO_KEY_DOWN:
-					key[KEY_DOWN] = true;
-					break;
-
-				case ALLEGRO_KEY_LEFT:
-					key[KEY_LEFT] = true;
-					break;
-
-				case ALLEGRO_KEY_RIGHT:
-					key[KEY_RIGHT] = true;
-					break;
+			break;
+		case 5:
+			cout << "YOU'RE IN ROOM 5." << endl;
+			cout << "There is nothing in here.It is a completely empty room." << endl;
+			cout << "You can go (north), (west), (south), and (east). " << endl;
+			monster(health, ammo);
+			getline(cin, input);
+			if (input.compare("west") == 0)
+				room = 6;
+			else if (input.compare("east") == 0)
+				room = 4;
+			else if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
+			}
+			else if (input.compare("south") == 0)
+				room = 2;
+			else if (input.compare("north") == 0)
+				if (items[9].compare("key") == 0) {
+					cout << "unlocked door" << endl;
+					room = 8;
 				}
-			}
-			else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
-				switch (ev.keyboard.keycode) {
-				case ALLEGRO_KEY_UP:
-					key[KEY_UP] = false;
-					break;
-
-				case ALLEGRO_KEY_DOWN:
-					key[KEY_DOWN] = false;
-					break;
-
-				case ALLEGRO_KEY_LEFT:
-					key[KEY_LEFT] = false;
-					break;
-
-				case ALLEGRO_KEY_RIGHT:
-					key[KEY_RIGHT] = false;
-					break;
-
-				case ALLEGRO_KEY_ESCAPE:
-					doexit = true;
-					break;
+				else if (input.compare("north") == 0 && items[9].compare("key") != 0) {
+					cout << "Not an option, door 8 is locked" << endl;
 				}
+				else
+					cout << "Invalid Input" << endl;
+			break;
+		case 6:
+			cout << "YOU'RE IN ROOM 6." << endl;
+			cout << "Seems like it is a storage closet." << endl;
+			cout << "The is a bunch of empty flask,test tubes,hot plate burners and other things you will see in a science lab environment." << endl;
+			cout << "You can go (north), (south), and (east). " << endl;
+			cout << "" << endl;
+			if (items[2].compare("barrel") != 0)
+				cout << "You find the barrel to the flamethrower. Type (barrel) to to pick it up." << endl;
+			getline(cin, input);
+			monster(health, ammo);
+
+			if (input.compare("north") == 0)
+				room = 9;
+			else if (input.compare("east") == 0)
+				room = 5;
+			else if (input.compare("south") == 0)
+
+				room = 3;
+
+			else if (input.compare("barrel") == 0) {
+				items[2] = "barrel";
+				cout << "You got the barrel" << endl;
+			}
+			else if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
+			}
+			else
+				cout << "Invalid input" << endl;
+			break;
+		case 7:
+			cout << "YOU'RE IN ROOM 7." << endl;
+			cout << "Room 7 is the place where they did experiments.It has cages,emergency cleanup,and lab equipment that seems broken and burned." << endl;
+			cout << "You can go (west), and (south). " << endl;
+			if (items[3].compare("lighter") != 0)
+				cout << "You find the ignition to the flamethrower. Type (lighter) to to pick it up." << endl;
+			getline(cin, input);
+			monster(health, ammo);
+			if (input.compare("south") == 0)
+				room = 4;
+			else if (input.compare("west") == 0 && items[9].compare("key") == 0) {
+				cout << "unlocked door" << endl;
+				room = 8;
+
+			}
+			else if (input.compare("west") == 0 && items[9].compare("key") != 0) {
+				cout << "Not an option, door 8 is locked" << endl;
 			}
 
-			if (redraw && al_is_event_queue_empty(event_queue)) {
-				redraw = false;
-
-				al_clear_to_color(al_map_rgb(0, 0, 0));
-
-				//this draws your map
-				al_draw_bitmap(map, -cameraX, -cameraY, 0);
-
-				//if you're not on the edge, draw the player in the middle!
-			if (onEdge == false)
-				al_draw_bitmap(bouncer, SCREEN_W / 2, SCREEN_H / 2, 0);
-			else//otherwise let him move!
-				al_draw_bitmap(bouncer, bouncer_x - cameraX, bouncer_y - cameraY, 0);
-
-
-				al_flip_display();
+			else if (input.compare("lighter") == 0) {
+				items[3] = "lighter";
+				cout << "You got the ignition to the flamethrower" << endl;
 			}
-		}
+			else if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
+			}
+			else
+				cout << "Invalid Input" << endl;
 
-		al_destroy_bitmap(bouncer);
-		al_destroy_timer(timer);
-		al_destroy_display(display);
-		al_destroy_event_queue(event_queue);
+			break;
+		case 8:
+			cout << "YOU'RE IN ROOM 8." << endl;
+			cout << "Room 8 is a locker rooom.Some of the lockers are open and some of the lockers are still locked." << endl;
+			cout << "You can go (north), (south), (west), and (east). " << endl;
+			getline(cin, input);
+			monster(health, ammo);
+			if (input.compare("north") == 0)
+				room = 10;
+			else if (input.compare("west") == 0)
+				room = 9;
+			else if (input.compare("south") == 0)
+				room = 5;
+			else if (input.compare("east") == 0)
+				room = 7;
+			else if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
+			}
+			else
+				cout << "not an option unless you are quiting" << endl;
+			break;
+		case 9:
+			cout << "YOU'RE IN ROOM 9." << endl;
+			cout << "Room 9 is a caferteria.It has circle tables and chairs with refrigerators lined up with drinks still in them.The kitchen is  in the right corner of the room." << endl;
+			cout << "You can go (south), and (east). " << endl;
+			getline(cin, input);
+			monster(health, ammo);
+			if (input.compare("south") == 0)
+				room = 6;
+			else if (input.compare("items") == 0) {// shows you your inventory
+				cout << "Your items in your inventory" << endl;
+				for (int i = 0; i<10; i++)
+					cout << items[i] << endl;
+			}
+			else if (input.compare("east") == 0 && items[9].compare("key") == 0) {
+				cout << "unlocked door" << endl;
+				room = 8;
+			}
+			else if (input.compare("east") == 0 && items[9].compare("key") != 0) {
+				cout << "Not an option, door 8 is locked" << endl;
+			}
+			else
+				cout << "Invalid Input" << endl;
+			break;
+		case 10:
+			cout << "YOU'RE IN ROOM 10." << endl;
+			cout << "Room 10 has big equipment and it looks like big experiments were performed in this room." << endl;
+			cout << "It has larger cleanup stations.Larger machines and larger cages." << endl;
 
-		return 0;
+			if (items[1].compare("gas") == 0 && items[2].compare("barrel") == 0 && items[3].compare("lighter") == 0 && items[4].compare("handle") == 0) {
+				cout << "You go in the nest and burn every one of those vicious eggs" << endl;
+				PlaySound(TEXT("fireloop.wav"), NULL, SND_FILENAME);
+				cout << "You win! " << endl;
+			}
+			else if (!items[1].compare("gas") == 0 && !items[2].compare("barrel") == 0 && !items[3].compare("lighter") == 0 && !items[4].compare("handle") == 0) {
+				cout << "A face hugger snuck up on you, attached to your face and now you are dead." << endl;
+				PlaySound(TEXT("screech.wav"), NULL, SND_FILENAME);
+				PlaySound(TEXT("scream.wav"), NULL, SND_FILENAME);
+				input = "quit";
+			}
+			return 0;
+			getline(cin, input);
+
+			break;
+		case 11:
+			cout << "description (reading the book)" << endl;
+			cout << "to stop reading type (pdown)." << endl;
+
+			if (items[9].compare("key") != 0) {//get the key to unlock room 8
+				cout << "There is a key on a the ground. Type (key) to to pick it up." << endl;
+			}
+			getline(cin, input);
+			if (input.compare("pdown") == 0)
+				room = 3;
+			else if (input.compare("key") == 0) {
+				items[9] = "key";
+				cout << "You got the key" << endl;
+			}
+			else
+				cout << "not a option" << endl;
+
+
+		}//switch
+	}//while loop
+	cout << "Game Over" << endl;
+}//ends main
+
+
+void monster(int health, int ammo) {
+	int number = rand() % 150 + 1;
+	if (number <= 50) {
+		cout << "A face hugger popped up but you shoot it with your deagle." << endl; \
+			health -= 100;
+		ammo -= 1;
+		cout << "health:" << health << endl;
+		cout << "ammo:" << ammo << endl;
 	}
+	else if (number>50 && number <= 100) {
+		cout << "You found ammo for your deagle!" << endl;
+		ammo += 2;
+	}
+}
